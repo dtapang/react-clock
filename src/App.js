d7934a1346f95f3ad969560ec0c14fe74dedffce
dtapang <dktapang@gmail.com>
2020-09-09 14:08:21 -0400

    React - Clock

--- src/App.js
+++ src/App.js
@@ -2,6 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function TimeElement(props) {
+    return(
+        <div>
+            <span> {props.hours} </span>:
+            <span> {props.minutes} </span>:
+            <span> {props.seconds} </span>
+            <span> {props.AM_PM} </span>
+        </div>
+    );
+}
+
+function EditTimeElement(props) {
+    return(
+        <div>
+            <span><input type="number" value={props.hours} /></span>:
+            <span><input type="number" value= {props.minutes} /></span>:
+            <span><input type="number" value= {props.seconds} /></span>
+            <span> {props.AM_PM} </span>
+        </div>
+    );
+}
+
+function Clock(props){
+    let h = props.hour;
+    let m = props.minute;
+    let s = props.second;
+    let am_pm = "AM";
+    if(h>=12){
+        am_pm = "PM";
+    }
+    return (
+        <TimeElement hours={h} minutes={m} seconds={s} AM_PM={am_pm}/>
+    );
+}
+
 function App() {
   return (
     <div className="App">
@@ -22,5 +57,4 @@ function App() {
     </div>
   );
 }
-
-export default App;
+export default Clock;
